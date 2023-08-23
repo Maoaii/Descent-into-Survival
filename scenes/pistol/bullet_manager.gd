6061e9292ba26101f1833bec830fac6f77b641ea
Girotto <l.girottoph@gmail.com>
2023-08-23 20:43:50 +0100

    spitting enemy working great

--- scenes/pistol/bullet_manager.gd
+++ scenes/pistol/bullet_manager.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 
-func handle_bullet_spawned(bullet: Bullet, pos: Vector2, dir: Vector2) -> void:
+func handle_bullet_spawned(bullet, pos: Vector2, dir: Vector2) -> void:
 	add_child(bullet)
 	bullet.global_position = pos
 	bullet.set_direction(dir)
